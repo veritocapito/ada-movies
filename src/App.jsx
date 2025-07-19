6f2ee78392b716347b7aac6b81b5962eafa7ac12
Veronica Capobianco <veritocapito@gmail.com>
2025-07-19 16:27:09 -0300

    add responsive navbar, custom font and icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 
+// Importamos nuestros componentes
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+// Importamos nuestras páginas
 import Home from './pages/Home';
+import Latest from './pages/Latest';
+import Popular from './pages/Popular';
+import Search from './pages/Search';
 
 function App() {
   return (
@@ -11,10 +17,13 @@ function App() {
       <div className="bg-gray-900 text-white min-h-screen flex flex-col">
         <Header />
 
-        <main className="flex-grow">
+        {/* El contenido principal ahora tiene un ancho máximo y está centrado */}
+        <main className="flex-grow w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <Routes>
             <Route path="/" element={<Home />} />
-
+            <Route path="/latest" element={<Latest />} />
+            <Route path="/popular" element={<Popular />} />
+            <Route path="/search" element={<Search />} />
           </Routes>
         </main>
 
